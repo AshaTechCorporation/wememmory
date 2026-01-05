4013e989cc44f995820f1199213af86d35cafef4
ChanuT3 <Chanuk_loveza@hotmail.com>
2026-01-05 16:51:49 +0700

    noti

--- lib/notification.dart
+++ lib/notification.dart
@@ -3,27 +3,22 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 class NotificationHelper {
-  
   // 1. เริ่มต้นระบบ (เหมือนเดิม)
   static Future<void> init() async {
-    await AwesomeNotifications().initialize(
-      null, 
-      [
-        NotificationChannel(
-          channelKey: 'daily_channel',
-          channelName: 'Daily Notifications',
-          channelDescription: 'แจ้งเตือนประจำเดือน',
-          defaultColor: Colors.deepPurple,
-          ledColor: Colors.white,
-          importance: NotificationImportance.High,
-          channelShowBadge: true,
-          locked: false,
-          criticalAlerts: Platform.isAndroid ? true : false, 
-          playSound: true,
-        )
-      ],
-      debug: true,
-    );
+    await AwesomeNotifications().initialize(null, [
+      NotificationChannel(
+        channelKey: 'daily_channel',
+        channelName: 'Daily Notifications',
+        channelDescription: 'แจ้งเตือนประจำเดือน',
+        defaultColor: Colors.deepPurple,
+        ledColor: Colors.white,
+        importance: NotificationImportance.High,
+        channelShowBadge: true,
+        locked: false,
+        criticalAlerts: Platform.isAndroid ? true : false,
+        playSound: true,
+      ),
+    ], debug: true);
   }
 
   // 2. ตรวจสอบสิทธิ์ (เหมือนเดิม)
@@ -73,30 +68,19 @@ class NotificationHelper {
     await AwesomeNotifications().cancelAllSchedules();
 
     String localTimeZone = await AwesomeNotifications().getLocalTimeZoneIdentifier();
-    
+
     await AwesomeNotifications().createNotification(
       content: NotificationContent(
-        id: 3000, 
-        channelKey: 'daily_channel', 
+        id: 3000,
+        channelKey: 'daily_channel',
         title: 'Wememory',
-        body: 'อย่าลืมเข้ามาบันทึกความทรงจำนะ', 
+        body: 'เดือนใหม่แล้ว อย่าลืมเข้ามาบันทึกความทรงจำนะ',
         notificationLayout: NotificationLayout.Default,
-        wakeUpScreen: true, 
-        category: NotificationCategory.Reminder, 
-        criticalAlert: Platform.isAndroid ? true : false, 
+        wakeUpScreen: true,
+        category: NotificationCategory.Reminder,
+        criticalAlert: Platform.isAndroid ? true : false,
       ),
-      schedule: NotificationCalendar(
-        day: 1,
-        hour: 10,
-        minute: 0,   
-        second: 0,
-        millisecond: 0,
-        
-        timeZone: localTimeZone, 
-        repeats: true,
-        allowWhileIdle: true, 
-        preciseAlarm: true,  
-      ),
+      schedule: NotificationCalendar(day: 1, hour: 10, minute: 0, second: 0, millisecond: 0, timeZone: localTimeZone, repeats: true, allowWhileIdle: true, preciseAlarm: true),
     );
     debugPrint("✅ ตั้งเวลาแจ้งเตือนทุกวันที่ 1 ของเดือน เวลา 10:00 เรียบร้อย");
   }
